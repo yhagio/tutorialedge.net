22a78e7ff2c48c0eda286177029a202e2ddf0722
= <emforce@live.com>
2017-09-24 21:15:21 +0100

    Updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 cd /var/www/test
 ls
 chmod +x scripts/hugo
-scripts/hugo --buildDrafts > output.log
+scripts/hugo --buildDrafts 
 rm -rf /var/www/html/public/
 mkdir /var/www/html/public/
 cp -R public/* /var/www/html/public/
