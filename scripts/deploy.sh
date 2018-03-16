8f73e555d03b65b6de1ead808c20bc21f27444f3
Elliot Forbes <emforce@live.com>
2018-03-16 13:27:47 +0000

    Another test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cd /var/www/html/public/
-aws s3 sync s3://test-tutorialedge-v2 .
+cd /var/www/test
+ls
+chmod +x scripts/hugo
+rm -rf /var/www/test/public
+scripts/hugo --buildDrafts 
+rm -rf /var/www/html/public/
+mkdir /var/www/html/public/
+cp -R public/* /var/www/html/public/
 
