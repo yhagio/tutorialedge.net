f528997169cc5c736c7727653c4e8d40901adbab
Elliot Forbes <emforce@live.com>
2018-03-16 13:09:24 +0000

    Testing s3 sync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-cd /var/www/test
-ls
-chmod +x scripts/hugo
-rm -rf /var/www/test/public
-scripts/hugo --buildDrafts 
-rm -rf /var/www/html/public/
-mkdir /var/www/html/public/
-cp -R public/* /var/www/html/public/
+cd /var/www/html/public/
+aws s3 sync s3://test-tutorialedge-v2 .
 
