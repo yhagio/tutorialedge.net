5c237870ecd088138b58102efba3fb59f00c09cd
Elliot Forbes <emforce@live.com>
2019-03-21 09:37:22 +0000

    Basic design for comments section

--- app/main.js
+++ app/main.js
@@ -1,11 +1,8 @@
-import Search from "./search/search.js";
 import Profile from "./profile/profile.js";
-import Utils from "./utils/utils.js";
 
 class Main {
   constructor() {
     // load in site utility script
-    this.utils = new Utils();
     // match corresponding script to 
     if (window.location.pathname === "/search/") {
       this.search = new Search();
