9c94349db42ed646677e2ac8b602fae4c44e5bdd
Elliot Forbes <emforce@live.com>
2020-02-16 18:00:07 +0000

    Moved profile styles into profile component

--- app/components/profile.ts
+++ app/components/profile.ts
@@ -17,19 +17,37 @@ class Profile extends PolymerElement{
     }
 
     static get template() {
-        return html`<div class="profile">
-                <img src="[[user.picture]]" class="profile-image" alt="Profile Picture">
+        return html`
+        <style>
+        .profile {
+            display: flex;
+        }
+        h1 {
+            margin-top: 20px;
+        }
     
-                <h2>Profile: [[user.name]] <br/>
-                <small>Thank you for Registering - This is currently in BETA</small></h2>
-            </div>
-    
-            <hr/>
-    
-            <p><b>Privacy Policy: <a href="/privacy/">Read Now</a></b></p>
-    
-            <a href="/logout/" class="btn btn-warning">Logout</a>
-            `;
+        img {
+            width: 80px;
+            height: 80px;
+            margin-right: 20px;
+            margin-top: 15px; 
+            border-radius: 50%;
+        }
+        </style>
+        
+        <div class="profile">
+            <img src="[[user.picture]]" alt="Profile Picture">
+
+            <h2>Profile: [[user.name]] <br/>
+            <small>Thank you for Registering - This is currently in BETA</small></h2>
+        </div>
+
+        <hr/>
+
+        <p><b>Privacy Policy: <a href="/privacy/">Read Now</a></b></p>
+
+        <a href="/logout/" class="btn btn-warning">Logout</a>
+        `;
     }
 
 }  
