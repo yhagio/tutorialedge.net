90b532ef2ba5e289144ffa0b7447181ed8ec5edb
Elliot Forbes <emforce@live.com>
2020-04-13 14:13:26 +0100

    Fixed comments...

--- app/config/production.js
+++ app/config/production.js
@@ -6,7 +6,7 @@ const config = {
     audience: 'https://' + 'tutorialedge.eu.auth0.com' + '/api/v2/', 
     responseType: 'token id_token',
     scope: 'openid profile',
-    apiBase: 'https://api.tutorialedge.net/api/'
+    apiBase: 'https://api.tutorialedge.net/api'
 }
 
 export default config;
