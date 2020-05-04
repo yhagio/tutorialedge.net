7c9f7247c3228bb6449607e45d3635ee2195338c
Elliot Forbes <emforce@live.com>
2020-05-04 17:26:01 +0100

    Updates to profile page

--- app/components/profile/Profile.vue
+++ app/components/profile/Profile.vue
@@ -8,27 +8,52 @@
                 <img v-bind:src="user.picture" alt="Profile Picture">
 
                 <h2>Profile: {{ this.user.name }}</h2>
-            </div>
-            
-            <h3>🏆 Trophies</h3>
+            </div> 
 
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="trophy">
-                        <img src="https://images.tutorialedge.net/images/golang.svg" alt="First 1000 Gophers!">
-                        <p><b>An Original Clan Member</b> - One of the First 1,000 Users</p>
+            <div class="clear"></div>
+        
+            <p><b>Privacy Policy: <a href="/privacy/">📕 Read Now</a></b></p>
+            <p><b> $100 Free DigitalOcean Credit: <a href="https://m.do.co/c/b348c7113c88">🦈 Claim Now</a></b></p>
+
+            <div class="profile-container">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="trophies-tab" data-toggle="tab" href="#trophies" role="tab" aria-controls="trophies" aria-selected="true">Trophies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="comments-tab" data-toggle="tab" href="#comments" role="tab" aria-controls="comments" aria-selected="false">Comments</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="trophies" role="tabpanel" aria-labelledby="trophies-tab">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="trophy">
+                                    <img src="https://images.tutorialedge.net/images/golang.svg" alt="First 1000 Gophers!">
+                                    <p><b>An Original Clan Member</b> - One of the First 1,000 Users</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="comments" role="tabpanel" aria-labelledby="comments-tab">
+                        <ul class="list-group">
+                            <li v-for="comment in comments" v-bind:key="comment.id" class="list-group-item d-flex justify-content-between align-items-center">
+                                <p><b>{{comment.slug}}</b> {{comment.body}}</p>
+                                <!-- <span class="badge badge-primary badge-pill">14</span> -->
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <div class="tab-pane fade" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+                        Currently Under Construction!
                     </div>
                 </div>
             </div>
 
-
             <hr/>
-            <p><b>Privacy Policy: <a href="/privacy/">📕 Read Now</a></b></p>
-
-            <div class="preferences">
-            </div>
-
-
             <a href="/logout/" class="btn btn-warning">Logout</a>
         </div>
     </div>
@@ -36,13 +61,25 @@
 
 <script>
 import * as Cookie from 'es-cookie';
+import axios from 'axios';
+import config from 'environment';
 
 export default {
     name: 'Profile',
     data: function() {
         return {
             user: {},
+            comments: [],
             loaded: false
+        }
+    },
+    methods: {
+        getComments: async function() {
+            let response = await axios.get(config.apiBase + "/v1/user", { params: {
+                name: this.user.name
+            }});
+
+            this.comments = response.data.comments;
         }
     },
     created: function() {
@@ -58,7 +95,7 @@ export default {
             this.$auth.login()
         } else {
             this.user = this.$auth.getUser()
-
+            this.getComments();
             let redirectTo = Cookie.get("redirectUri");
             if(redirectTo) {
                 Cookie.remove("redirectUri")
@@ -73,6 +110,9 @@ export default {
 </script>
 
 <style lang="scss">
+.tab-content {
+    margin-top: 20px; 
+}
 .profile-wrapper {
     h3 {
         margin-left: 0px;
