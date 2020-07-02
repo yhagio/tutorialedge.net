0333a66e28c9097520a132a2037c990b8b39fdb0
Elliot Forbes <emforce@live.com>
2020-07-02 18:06:04 +0100

    Stripe integration

--- app/components/profile/ProfilePricing.vue
+++ app/components/profile/ProfilePricing.vue
@@ -1,11 +1,66 @@
 <template>
-    <div>
-        <h2>Pricing</h2>
+    <div class="row">
+        <div class="col-lg-6">
+            <button class='btn btn-primary pay-with-stripe' @click='pay'>Pay with credit card</button>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "ProfilePricing"
+    name: "ProfilePricing",
+    props: ['user'],
+    pay () {
+      let stripe = Stripe('pk_test_vzLRvltMJHJJ65YFypICxZrY');
+
+      stripe.redirectToCheckout({
+          lineItems: [
+              {price: 'price_1H0UFdH6SNauSNAXRNZxJgGe', quantity: 1}
+          ],
+          mode: 'subscription',
+          successUrl: 'http://localhost:1313/profile/',
+          cancelUrl: 'http://localhost:1313/profile/'
+      })
+        .then(data => {
+            console.log(data.token);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
 }
 </script>
+
+<style lang="scss">
+/**
+ * The CSS shown here will not be introduced in the Quickstart guide, but shows
+ * how you can use CSS to style your Element's container.
+ */
+.StripeElement {
+  box-sizing: border-box;
+
+  height: 40px;
+
+  padding: 10px 12px;
+
+  border: 1px solid transparent;
+  border-radius: 4px;
+  background-color: white;
+
+  box-shadow: 0 1px 3px 0 #e6ebf1;
+  -webkit-transition: box-shadow 150ms ease;
+  transition: box-shadow 150ms ease;
+}
+
+.StripeElement--focus {
+  box-shadow: 0 1px 3px 0 #cfd7df;
+}
+
+.StripeElement--invalid {
+  border-color: #fa755a;
+}
+
+.StripeElement--webkit-autofill {
+  background-color: #fefde5 !important;
+}
+</style>
---
+++ app/components/profile/ProfileSettings.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Profile-Pricing />
+
+        <!-- <Profile-Delete-Button /> -->
+    </div>
+</template>
+
+<script>
+import ProfilePricing from './ProfilePricing.vue';
+import ProfileDeleteButton from './ProfileDeleteButton.vue';
+
+export default {
+    name: "ProfileSettings",
+    props: ['user'],
+    components: {
+        ProfilePricing,
+        ProfileDeleteButton
+    },
+    data: function() {
+        return {
+            user: {}
+        }
+    },
+    mounted: function() {
+        console.log("hi!");
+    }
+}
+</script>
