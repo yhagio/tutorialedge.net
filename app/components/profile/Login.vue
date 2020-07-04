56757d9112e9bba3c060c51fa83fc54ff098c0d7
Elliot Forbes <emforce@live.com>
2020-07-04 21:55:12 +0100

    Updates

--- app/components/profile/Login.vue
+++ app/components/profile/Login.vue
@@ -1,34 +1,38 @@
 <template>
-     <div class="login-box">
-        <div class="login">	
-            <div></div>
-            <div class="price-box">
-                <h2>Register or Sign in 🚀</h2>
-                <p>Authenticate with Google or GitHub:</p>
-                <button class="btn btn-primary" v-on:click="this.signup">🚀 Click Here</button>
-            </div>
-            <div></div>
-        </div>
+    <div class="login-box">
+        <img class="logo" src="https://images.tutorialedge.net/images/logo.svg" alt="TutorialEdge Logo" />
+        <h2>Redirecting You to Auth0</h2>
     </div>
 </template>
 
 <script>
 export default {
     name: "Login",
-    methods: {
-        signup: function() {
-            this.$auth.login()
-        }
+    mounted: function() {
+        this.$auth.login()
     }
 }
 </script>
 
 <style lang="scss" scoped>
 
+.logo {
+    width: 75px;
+    margin-bottom: 20px;
+    height: auto;
+}
+
+h2 {
+    font-size: 1.875rem;
+    margin-bottom: 20px;
+}
+
 .login-box {
   padding: 40px;
   background-image: url(/images/svg/small-dots.svg);
   min-height: 50vh;
+  background-color: #F9FAFB;
+  text-align: center;
   h3 {
       background-color: #F5F7F9;
       padding: 20px;
@@ -39,49 +43,39 @@ export default {
       text-decoration: none !important;
   }
     
-  .login {
-      display: grid;
-      justify-content: center;
-      grid-gap: 1px;
-      grid-template-columns: repeat(3, 1fr);
+  .login-cont {
+    margin: auto;
+    width: 600px;
+    padding: 40px;
+    background-color: white;
+    border-radius: 8px;
+    -webkit-box-shadow: 0 2px 4px 0 rgba(14,30,37,.12);
+    box-shadow: 0 2px 4px 0 rgba(14,30,37,.12);
+    color: rgba(14,30,37,.54);
+    text-align: center;
+    h2 {
+        font-size: 20px;
+        small {
+            font-weight: 800;
+            color: #999;
+            clear: both;
+            margin-bottom: 15px;
+        }
+    }
 
-      .price-box {
-          flex: 0 0 32%;  
-          margin: 20px;
-          margin-top: 40px;
-          padding: 40px;
-          align-self: center;
-          background-color: white;
-          border-radius: 8px;
-          -webkit-box-shadow: 0 2px 4px 0 rgba(14,30,37,.12);
-          box-shadow: 0 2px 4px 0 rgba(14,30,37,.12);
-          color: rgba(14,30,37,.54);
-          text-align: center;
-          h2 {
-              font-size: 20px;
-              small {
-                  font-weight: 800;
-                  color: #999;
-                  clear: both;
-                  margin-bottom: 15px;
-              }
-          }
+    p {
+        font-size: 1rem;
+    }
 
-          p {
-              font-size: 1rem;
-          }
+    button {
+        margin: 0;
+    }
 
-          button {
-              margin: 0;
-          }
+    .btn-sponsor {
+        border: 1px solid #3991FA;
+        color: #3991FA;
+    }
 
-          .btn-sponsor {
-              border: 1px solid #3991FA;
-              color: #3991FA;
-          }
-
-      }
   }
-
 }
 </style>
